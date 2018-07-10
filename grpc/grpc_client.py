17a4d5db6b57eb5393651a0da76245297beef6ed
Eder Trindade <eder.gabriel.felix@gmail.com>
2018-07-09 23:29:05 -0300

    Added data analysis, removed unused files

--- grpc/grpc_client.py
+++ grpc/grpc_client.py
@@ -1,18 +1,3 @@
-# Copyright 2015 gRPC authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""The Python implementation of the GRPC helloworld.Greeter client."""
-
 from __future__ import print_function
 
 import grpc
@@ -33,25 +18,6 @@ def run():
     stub = functions_pb2_grpc.EpFunctionsStub(channel)
     from datetime import datetime
     print (str(datetime.now())) 
-
-
-def timerfunc(func):
-    """
-    A timer decorator
-    """
-    def function_timer(*args, **kwargs):
-        """
-        A nested function for timing other functions
-        """
-        start = time.time()
-        value = func(*args, **kwargs)
-        end = time.time()
-        runtime = end - start
-        msg = "The runtime for {func} took {time} seconds to complete"
-        print(msg.format(func=func.__name__,
-                         time=runtime))
-        return value
-    return function_timer
 
 emp = functions.Employee()
 
@@ -89,4 +55,4 @@ def run_benchmark():
 if __name__ == '__main__':
     run()
     run_benchmark()
-  
+
--- grpc/grpc_server.py
+++ grpc/grpc_server.py
@@ -1,43 +1,14 @@
-# Copyright 2015 gRPC authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""The Python implementation of the GRPC helloworld.Greeter server."""
-
 from concurrent import futures
 import time
 
 
 import grpc
 
-import helloworld_pb2
-import helloworld_pb2_grpc
 import functions_pb2
 import functions_pb2_grpc
 import functions
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
-
-
-class Greeter(helloworld_pb2_grpc.GreeterServicer):
-
-    def SayHello(self, request, context):
-        from datetime import datetime
-        datetime.now()
-        request.name = request.name + ' '+ str(datetime.now())
-        return helloworld_pb2.HelloReply(message='Hello, %s!' %request.name)
-    def SayHelloAgain(self, request, context):
-        return helloworld_pb2.HelloReply(message='Hello again, %s!' % request.name)
-
 
 class EpFunctions(functions_pb2_grpc.EpFunctionsServicer):
     from functions import Employee 
@@ -94,7 +65,6 @@ class EpFunctions(functions_pb2_grpc.EpFunctionsServicer):
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
     #adicionando os servicos do EP ao servidor
     functions_pb2_grpc.add_EpFunctionsServicer_to_server(EpFunctions(), server)
     server.add_insecure_port('[::]:50051')
